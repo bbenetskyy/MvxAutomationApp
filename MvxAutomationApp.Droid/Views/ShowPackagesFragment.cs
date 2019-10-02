297def6708f0be7411a9f817bf417233a35b19d6
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2019-10-02 11:24:01 +0200

    Add Enter and Show Packages Fragments

---
+++ MvxAutomationApp.Droid/Views/ShowPackagesFragment.cs
@@ -0,0 +1,20 @@
+﻿using Android.OS;
+using Android.Views;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvxAutomationApp.Core.ViewModels;
+
+namespace MvxAutomationApp.Droid.Views
+{
+    [MvxFragmentPresentation(typeof(MainContainerViewModel), Resource.Id.content_frame, true)]
+    public class ShowPackagesFragment : MvxFragment<ShowPackagesViewModel>
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            return this.BindingInflate(Resource.Layout.fragment_show_packages, container, false);
+        }
+    }
+}
