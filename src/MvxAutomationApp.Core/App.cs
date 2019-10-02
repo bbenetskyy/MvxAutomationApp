e32c602f456b933c962ea3fcf151cec75f7dca4a
Bohdan Benetskyi (PGS Software) <bbenetskyi@pgs-soft.com>
2019-10-02 10:09:51 +0200

    Add project files.

---
+++ src/MvxAutomationApp.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using MvxAutomationApp.Core.ViewModels.Main;
+
+namespace MvxAutomationApp.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<MainViewModel>();
+        }
+    }
+}
---
+++ src/MvxAutomationApp.Droid/Setup.cs
@@ -0,0 +1,9 @@
+﻿using MvvmCross.Droid.Support.V7.AppCompat;
+using MvxAutomationApp.Core;
+
+namespace MvxAutomationApp.Droid
+{
+    public class Setup : MvxAppCompatSetup<App>
+    {
+    }
+}
